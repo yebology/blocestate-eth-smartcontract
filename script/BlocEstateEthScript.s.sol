f938fae8c0f0a2367fe3c810b041a4ecfb7bef39
yebology <139052533+yebology@users.noreply.github.com>
2024-12-08 15:37:34 +0700

    done deploy script

--- script/BlocEstateEthScript.s.sol
+++ script/BlocEstateEthScript.s.sol
@@ -3,9 +3,21 @@
 pragma solidity ^0.8.28;
 
 import { Script } from "forge-std/Script.sol";
+import { BlocEstateEth } from "../src/BlocEstateEth.sol";
 
 contract BlocEstateEthScript is Script {
     // 
     
+    event BlocEstateEthCreated(address indexed blocEstateEth);
+
+    function run() external returns (BlocEstateEth) {
+        vm.startBroadcast();
+        BlocEstateEth blocEstateEth = new BlocEstateEth();
+        vm.stopBroadcast();
+        
+        emit BlocEstateEthCreated(address(blocEstateEth));
+        return blocEstateEth;
+    }
+
     //
 }
