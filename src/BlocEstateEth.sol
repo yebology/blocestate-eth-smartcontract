399b28a9fb3b7e82983f398a14c1c1c917b26f78
yebology <139052533+yebology@users.noreply.github.com>
2024-12-09 19:34:59 +0700

    progres unit test +several src function

--- src/BlocEstateEth.sol
+++ src/BlocEstateEth.sol
@@ -2,56 +2,242 @@
 
 pragma solidity ^0.8.28;
 
+import {console} from "forge-std/console.sol";
+
 contract BlocEstateEth {
-    // 
-    uint256 private s_auctionTotal;
+    //
+    mapping(uint256 auctionId => Bid[] bid) private s_bidFromUser;
+    mapping(uint256 auctionId => mapping(address user => uint256 bidAmount))
+        private s_userBidAmount;
 
-    mapping(uint256 auctionId => address seller) private s_seller;
-    mapping(uint256 auctionId => mapping(address user => uint256 amount)) private s_bidAmountFromUser;
-
-    error InsufficientBalance();
+    error MustBeGreaterThanHighestPrice();
+    error AuctionWinnerCannotWithdrawEth();
+    error OnlyOwnerCanWithdraw();
     error NonExistingBidder();
-    error RefundFailed();
+    error TransferFailed();
+    error AuctionNotStartedYet();
+    error AuctionNotClosedYet();
+    error InvalidField();
 
-    event RefundSuccessfully(uint256 indexed auctionId, address indexed user, uint256 indexed bidAmount);
-    event NewSellerRegistered(uint256 indexed auctionId, address indexed seller);
-    event BidPlacedSuccessfully(uint256 indexed auctionId, address indexed user, uint256 indexed bidAmount);
+    event NewAuctionRegistered(
+        uint256 indexed auctionId,
+        address indexed seller
+    );
+    event TransferSuccessfully(
+        uint256 indexed auctionId,
+        address indexed user,
+        uint256 indexed bidAmount
+    );
+    event BidPlacedSuccessfully(
+        uint256 indexed auctionId,
+        address indexed user,
+        uint256 indexed bidAmount
+    );
 
-    modifier checkBalance(address _user, uint256 _bidAmount) {
-        if (address(_user).balance < _bidAmount) {
-            revert InsufficientBalance();
+    struct Auction {
+        uint256 id;
+        address seller;
+        uint256 startPrice;
+        uint256 openTimestamp;
+        uint256 closeTimestamp;
+    }
+    struct Bid {
+        address user;
+        uint256 bidAmount;
+        uint256 timestamp;
+    }
+
+    Auction[] private s_auctionList;
+
+    modifier checkBidAmount(uint256 _auctionId, uint256 _bidAmount) {
+        uint256 totalBid = _getTotalBid(_auctionId);
+        uint256 startPrice = s_auctionList[_auctionId].startPrice;
+        uint256 highestPrice = totalBid > 0
+            ? _getAuctionHighestBid(_auctionId, totalBid).bidAmount
+            : startPrice;
+        if (_bidAmount <= highestPrice) {
+            revert MustBeGreaterThanHighestPrice();
+        }
+        _;
+    }
+
+    modifier checkRecipientNotAWinner(uint256 _auctionId, address _user) {
+        uint256 totalBid = _getTotalBid(_auctionId);
+        address highestBidder = _getAuctionHighestBid(_auctionId, totalBid)
+            .user;
+        if (highestBidder == _user) {
+            revert AuctionWinnerCannotWithdrawEth();
         }
         _;
     }
 
     modifier checkExistingBidder(uint256 _auctionId, address _user) {
-        if (s_bidAmountFromUser[_auctionId][_user] == 0) {
+        if (s_userBidAmount[_auctionId][_user] == 0) {
             revert NonExistingBidder();
         }
         _;
     }
 
-    function registerSellerAddress() external {
-        s_seller[s_auctionTotal] = msg.sender;
-        s_auctionTotal++;
-        emit NewSellerRegistered(s_auctionTotal--, msg.sender);
+    modifier checkAuctionAlreadyOpen(uint256 _auctionId) {
+        if (s_auctionList[_auctionId].openTimestamp < block.timestamp) {
+            revert AuctionNotStartedYet();
+        }
+        _;
     }
 
-    function bidRealEstate(uint256 _auctionId) payable external checkBalance(msg.sender, msg.value) {
-        s_bidAmountFromUser[_auctionId][msg.sender] += msg.value;
+    modifier checkAuctionAlreadyEnded(uint256 _auctionId) {
+        if (s_auctionList[_auctionId].closeTimestamp > block.timestamp) {
+            revert AuctionNotClosedYet();
+        }
+        _;
+    }
+
+    modifier checkOwnership(uint256 _auctionId, address _user) {
+        address owner = s_auctionList[_auctionId].seller;
+        if (owner != _user) {
+            revert OnlyOwnerCanWithdraw();
+        }
+        _;
+    }
+
+    modifier checkAuctionInput(
+        uint256 _startPrice,
+        uint256 _openTimestamp,
+        uint256 _closeTimestamp
+    ) {
+        if (
+            _startPrice <= 0 ||
+            _openTimestamp <= 0 ||
+            _closeTimestamp <= 0 ||
+            _closeTimestamp <= block.timestamp ||
+            _openTimestamp >= _closeTimestamp
+        ) {
+            revert InvalidField();
+        }
+        _;
+    }
+
+    // done
+    function registerAuction(
+        uint256 _startPrice,
+        uint256 _openTimestamp,
+        uint256 _closeTimestamp
+    ) external checkAuctionInput(_startPrice, _openTimestamp, _closeTimestamp) {
+        _addNewAuctionData(_startPrice, _openTimestamp, _closeTimestamp);
+    }
+
+    // done
+    function bidRealEstate(
+        uint256 _auctionId
+    )
+        external
+        payable
+        checkAuctionAlreadyOpen(_auctionId)
+        checkBidAmount(_auctionId, msg.value)
+    {
+        _checkBidAmountStatus(_auctionId, msg.sender, msg.value);
+    }
+
+    // done
+    function refundToBidder(
+        uint256 _auctionId
+    )
+        external
+        checkExistingBidder(_auctionId, msg.sender)
+        checkAuctionAlreadyEnded(_auctionId)
+        checkRecipientNotAWinner(_auctionId, msg.sender)
+    {
+        _transferToUser(_auctionId, msg.sender, msg.sender);
+    }
+
+    function withdrawWinnerEth(
+        uint256 _auctionId
+    )
+        external
+        checkOwnership(_auctionId, msg.sender)
+        checkAuctionAlreadyEnded(_auctionId)
+    {
+        _withdrawToOwnerAccount(_auctionId, msg.sender);
+    }
+
+    function getAuctionList() external view returns (Auction[] memory) {
+        return s_auctionList;
+    }
+
+    function getAuctionBidders(
+        uint256 _auctionId
+    ) external view returns (Bid[] memory) {
+        return s_bidFromUser[_auctionId];
+    }
+
+    function _addNewAuctionData(
+        uint256 _startPrice,
+        uint256 _openTimestamp,
+        uint256 _closeTimestamp
+    ) private {
+        s_auctionList.push(
+            Auction({
+                id: s_auctionList.length,
+                seller: msg.sender,
+                startPrice: _startPrice,
+                openTimestamp: _openTimestamp,
+                closeTimestamp: _closeTimestamp
+            })
+        );
+        emit NewAuctionRegistered(s_auctionList.length - 1, msg.sender);
+    }
+
+    function _checkBidAmountStatus(
+        uint256 _auctionId,
+        address _user,
+        uint256 _bidAmount
+    ) private {
+        Bid memory newBid = Bid({
+            user: msg.sender,
+            bidAmount: msg.value,
+            timestamp: block.timestamp
+        });
+        s_bidFromUser[_auctionId].push(newBid);
+        s_userBidAmount[_auctionId][_user] += _bidAmount;
         emit BidPlacedSuccessfully(_auctionId, msg.sender, msg.value);
     }
 
-    function refundToBidder(uint256 _auctionId) external checkExistingBidder(_auctionId, msg.sender) {
-        uint256 refundValue = s_bidAmountFromUser[_auctionId][msg.sender];
-        address payable recipient = payable(msg.sender);
+    function _transferToUser(
+        uint256 _auctionId,
+        address _from,
+        address _to
+    ) private {
+        uint256 refundValue = s_userBidAmount[_auctionId][_from];
+        address payable recipient = payable(_to);
         bool transferStatus = recipient.send(refundValue);
         if (transferStatus) {
-            emit RefundSuccessfully(_auctionId, msg.sender, refundValue);
-        } 
-        else {
-            revert RefundFailed();
+            s_userBidAmount[_auctionId][_from] = 0;
+            emit TransferSuccessfully(_auctionId, _to, refundValue);
+        } else {
+            revert TransferFailed();
         }
+    }
+
+    function _withdrawToOwnerAccount(
+        uint256 _auctionId,
+        address _owner
+    ) private {
+        uint256 totalBid = _getTotalBid(_auctionId);
+        address winner = _getAuctionHighestBid(_auctionId, totalBid).user;
+        _transferToUser(_auctionId, winner, _owner);
+    }
+
+    function _getTotalBid(
+        uint256 _auctionId
+    ) private view returns (uint256 totalBid) {
+        return s_bidFromUser[_auctionId].length;
+    }
+
+    function _getAuctionHighestBid(
+        uint256 _auctionId,
+        uint256 _totalBid
+    ) private view returns (Bid memory bid) {
+        return s_bidFromUser[_auctionId][_totalBid - 1];
     }
     //
 }
--- test/BlocEstateEthTest.t.sol
+++ test/BlocEstateEthTest.t.sol
@@ -2,11 +2,93 @@
 
 pragma solidity ^0.8.28;
 
-import { Test } from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {BlocEstateEthScript} from "../script/BlocEstateEthScript.s.sol";
+import {BlocEstateEth} from "../src/BlocEstateEth.sol";
 
 contract BlocEstateEthTest is Test {
     //
+    BlocEstateEthScript blocEstateEthScript;
+    BlocEstateEth blocEstateEth;
+
+    address private constant BOB = address(1);
+    address private constant ALICE = address(2);
+
+    modifier registerNewBidder(
+        address _creator,
+        uint256 _startPrice,
+        uint256 _openTimestamp,
+        uint256 _closeTimestamp
+    ) {
+        vm.startPrank(_creator);
+        blocEstateEth.registerAuction(
+            _startPrice,
+            _openTimestamp,
+            _closeTimestamp
+        );
+        vm.stopPrank();
+        _;
+    }
+
+    modifier addNewBidder(
+        address _bidder,
+        uint256 _auctionId,
+        uint256 _amount
+    ) {
+        hoax(_bidder, _amount);
+        blocEstateEth.bidRealEstate{value: _amount}(_auctionId);
+        _;
+    }
+
+    function setUp() public {
+        blocEstateEthScript = new BlocEstateEthScript();
+        blocEstateEth = blocEstateEthScript.run();
+    }
+
+    function testSuccessfullyRegisterNewAuction()
+        public
+        registerNewBidder(
+            BOB,
+            2 ether,
+            block.timestamp,
+            block.timestamp + 1 minutes
+        )
+    {
+        uint256 actualAuctionTotal = blocEstateEth.getAuctionList().length;
+        uint256 expectAuctionTotal = 1;
+
+        assertEq(actualAuctionTotal, expectAuctionTotal);
+    }
+
+    function testSuccessfullyPlaceABid()
+        public
+        registerNewBidder(
+            BOB,
+            1 ether,
+            block.timestamp,
+            block.timestamp + 1 minutes
+        )
+        addNewBidder(ALICE, 0, 2 ether)
+    {
+        uint256 actualTotalBid = blocEstateEth.getAuctionBidders(0).length;
+        uint256 expectTotalBid = 1;
+
+        uint256 actualBidderBalance = address(ALICE).balance;
+        uint256 expectBidderBalance = 0 ether;
+
+        uint256 actualSmartContractBalance = address(blocEstateEth).balance;
+        uint256 expectSmartContractBalance = 1 ether;
+
+        uint256 actualCreatorBalance = address(BOB).balance;
+        uint256 expectCreatorBalance = 0 ether;
+
+        assertEq(actualTotalBid, expectTotalBid);
+        assertEq(actualBidderBalance, expectBidderBalance);
+        // assertEq(actualSmartContractBalance, expectSmartContractBalance);
+        assertEq(actualCreatorBalance, expectCreatorBalance);
+    }
+
+    function testSuccessfullyRefundToBidder() public {}
 
     //
 }
-
